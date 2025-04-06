5f9c0b6327e714988f6e9ddc7af8c715bb616b81
zadorim <zadorim@kkszki.hu>
2025-04-06 18:14:57 +0200

    Osztály szerkesztő modal módosítása, kisebb javítások az App.jsx fájlban.

--- frontend/src/pages/OsztalyokOldal.jsx
+++ frontend/src/pages/OsztalyokOldal.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import OsztalySzerkesztoModal from '../components/OsztalySzerkesztoModal';
 
 function OsztalyokOldal() {
   const [osztalyok, setOsztalyok] = useState([]);
